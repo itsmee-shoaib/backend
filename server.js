2243050bbaa722d886d3e194fa8539bdc2942f60
itsmee-shoaib <itsmeeshoaib1@gmail.com>
2025-01-11 11:20:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const subjectRoutes = require('./routes/subjectRoutes');
 const quizRoutes = require('./routes/quizRoutes');
-const userRouter=require('./routes/userRoutes');
+const userRouter = require('./routes/userRoutes');
 const path = require('path');
 
 
@@ -15,14 +15,16 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
-app.use('/api/auth',authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/subjects', subjectRoutes);
 app.use('/api/quizzes', quizRoutes);
-app.use('/api/users',userRouter);
+app.use('/api/users', userRouter);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 60000, // 60 seconds
+}).then(() => { console.log('connected') }).catch(error=>console.log(error));
 
 app.listen(5000, () => console.log('Server running on port 5000'));
